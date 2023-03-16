a2f88289364667a700013a7d6f77f95e991de730
Mohmed S Shaikh <sahilshaikh1595@gmail.com>
2023-03-16 13:14:55 -0400

    updated server link

--- client/script.js
+++ client/script.js
@@ -73,7 +73,7 @@ const handleSubmit = async (e) => {
 
   loader(messageDiv);
 
-  const response = await fetch("http://localhost:5000", {
+  const response = await fetch("https://openai-chatbot-shaikh.onrender.com", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
